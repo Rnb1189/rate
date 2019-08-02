8a7ae08730c6bc9d98776552f891d4196fa72a18
Rnb1189 <omid.abdollahi.1390@gmail.com>
2019-08-02 19:49:18 +0430

    d r

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,11 @@
 /* eslint no-console: 0 */
-import Rate from 'rc-rate';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../assets/index.less';
+import Rate from "rc-rate";
+import React from "react";
+import ReactDOM from "react-dom";
+import "../assets/index.less";
 
 function onChange(v) {
-  console.log('selected star', v);
+  console.log("selected star", v);
 }
 
 ReactDOM.render(
@@ -14,6 +14,15 @@ ReactDOM.render(
       defaultValue={2.5}
       onChange={onChange}
       style={{ fontSize: 40 }}
+      allowHalf
+      allowClear={false}
+    />
+    <br />
+    <Rate
+      isRtl
+      defaultValue={0}
+      onChange={onChange}
+      style={{ fontSize: 80 }}
       allowHalf
       allowClear={false}
     />
@@ -40,5 +49,6 @@ ReactDOM.render(
       style={{ fontSize: 50, marginTop: 24 }}
       character={<i className="anticon anticon-star" />}
     />
-  </div>
-, document.getElementById('__react-content'));
+  </div>,
+  document.getElementById("__react-content")
+);
